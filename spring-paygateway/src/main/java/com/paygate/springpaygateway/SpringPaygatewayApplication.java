5ddcc55fd41037347d3c33346d10b5e477627db7
nu20106899 <nu20106899@10.156.238.120>
2020-02-05 20:18:08 +0530

    Initial commit

---
+++ spring-paygateway/src/main/java/com/paygate/springpaygateway/SpringPaygatewayApplication.java
@@ -0,0 +1,13 @@
+package com.paygate.springpaygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaygatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPaygatewayApplication.class, args);
+	}
+
+}
